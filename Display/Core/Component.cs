61046b1c0338f84ad572c617764d59b58a77cb3f
Jitze <jitze.1ceusters@gmail.com>
2023-12-29 11:09:10 +0100

    Added Component and Data class to PROJECT/DISPLAY/CORE

---
+++ Display/Core/Component.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceusters_Jitze_GameDevelopment.Display.Core
+{
+    public abstract class Component
+    {
+        public abstract void LoadContent(ContentManager Content);
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
---
+++ Display/Core/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceusters_Jitze_GameDevelopment.Display.Core
+{
+    public class Data
+    {
+        public static bool Exit { get; set; } = false;
+        public enum Scenes { Menu, Level, GameOver, Game}
+        public static Scenes CurrentScene { get; set; } = Scenes.Menu;
+    }
+}
